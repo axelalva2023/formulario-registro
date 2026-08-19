f7a7c2b2f88fce1ec9cac6cec3eef80fece7dd6c
Axel Alva <axelalva2021@gmail.com>
2026-08-18 21:17:21 -0300

    cambiosMain

--- backend/src/main.js
+++ backend/src/main.js
@@ -10,7 +10,9 @@ const servidor = express();
 
 servidor.use(cors());
 
-const http = servidor.listen("3000", () => console.log("Corriendo servidor de manera exitosa")); mongoose.connect("mongodb://axelalva:AQY0efUw57tGflSJ@ac-5rdxobo-shard-00-00.bcryxcu.mongodb.net:27017,ac-5rdxobo-shard-00-01.bcryxcu.mongodb.net:27017,ac-5rdxobo-shard-00-02.bcryxcu.mongodb.net:27017/?ssl=true&replicaSet=atlas-1159a3-shard-0&authSource=admin&appName=Backend1", {dbName: "tienda"}).then(() => console.log("Conectado a Mongo"))
+const PORT = process.env.PORT || 3000;
+
+const http = servidor.listen(PORT, () => console.log("Corriendo servidor de manera exitosa")); mongoose.connect("mongodb://axelalva:AQY0efUw57tGflSJ@ac-5rdxobo-shard-00-00.bcryxcu.mongodb.net:27017,ac-5rdxobo-shard-00-01.bcryxcu.mongodb.net:27017,ac-5rdxobo-shard-00-02.bcryxcu.mongodb.net:27017/?ssl=true&replicaSet=atlas-1159a3-shard-0&authSource=admin&appName=Backend1", {dbName: "tienda"}).then(() => console.log("Conectado a Mongo"))
 
 servidor.use(express.json());
 
